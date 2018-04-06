4c9b5b0dad20776a7aa20cd452719e3dcdbccd07
Xuan Mai <xtm1109@westminstercollege.edu>
2018-04-06 20:28:45 +0700

    Update getUser

--- user_api/getUser.php
+++ user_api/getUser.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Content-Type: application/json; charset=UTF-8");
+session_start();
 
 include_once '../DbConnection.php';
 include_once '../User.php';
@@ -10,10 +10,39 @@ $conn = $db_conn->getConnection();
 
 $user = new User($conn);
 
-$statement = $user->getUser();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($result, JSON_PRETTY_PRINT);
+if(isset($_POST['getUser'])){
+	$username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+	$password = !empty($_POST['password']) ? trim($_POST['password']) : null;
+	$email = !empty($_POST['user_email']) ? trim($_POST['user_email']) : null;
 
-echo $json;
+	$isAuthenticated = $user->userLogin($username, $password);
+	if ($isAuthenticated == 1) {
+		$statement = $user->getUser($email);
+		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		$json = json_encode($result, JSON_PRETTY_PRINT);
 
+		echo '<pre>' . $json . '</pre>';
+	}
+}
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Get User</title>
+    </head>
+    <body>
+        <h1>Authenticate to get User</h1>
+        <form action="getUser.php" method="post">
+                <label>Username</label>
+                <input type="text" name="username"><br>
+                <label>Password</label>
+                <input type="password" name="password"><br>
+                <label>User email to look up</label>
+                <input type="text" name="user_email"><br>
+
+            	<input type="submit" name="getUser" value="Get User">
+        </form>
+    </body>
+</html>
+
